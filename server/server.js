56f287ff13be2b6f47ecbc1c8687b9de98e22367
ef56 <efargas56@gmail.com>
2022-07-28 22:43:57 -0400

    book search engine

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+});
